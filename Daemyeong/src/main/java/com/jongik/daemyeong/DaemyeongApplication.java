9c741da84084a950f937508f4813d927093ba3af
박종익 <pji3504@naver.com>
2021-06-15 16:25:16 +0900

    test

---
+++ Daemyeong/src/main/java/com/jongik/daemyeong/DaemyeongApplication.java
@@ -0,0 +1,13 @@
+package com.jongik.daemyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaemyeongApplication.class, args);
+	}
+
+}
---
+++ Daemyeong/src/test/java/com/jongik/daemyeong/DaemyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongik.daemyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaemyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
